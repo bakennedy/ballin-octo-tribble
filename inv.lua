22585248bb35ab2d4c5dea7388c99b534580a73f
Brian Kennedy <brian.alex.kennedy@gmail.com>
2015-06-09 01:33:18 -0700

    readyItem now scans the inventory for a given item

--- inv.lua
+++ inv.lua
@@ -1,16 +1,13 @@
 function readyItem(item)
 	local slot, count
-	repeat
+	for i=1,16
 		slot = turtle.getSelectedSlot()
 		count = turtle.getItemCount(slot)
 		detail = turtle.getItemDetail()
-		if count == 0 or not detail.name == item then
-			if slot == 16 then 
-				return false 
-			end
-			turtle.select(slot+1)
-		end
-	until (count > 0  detail.name == item) and  or slot == 16
-	return slot
+		if count > 0 and detail.name == item then
+		    return slot
+	    end
+		turtle.select((slot + 1) % 16 + 1)
+	end
 end
 
